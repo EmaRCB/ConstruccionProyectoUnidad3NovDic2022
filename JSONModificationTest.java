f0165d282a0af87efe7bd1e67a71243f0e7f631d
EmaRCB <A17003636@alumnos.uady.mx>
2022-12-05 19:57:37 -0600

    pruebas listas

--- JSONModificationTest.java
+++ JSONModificationTest.java
@@ -45,37 +45,29 @@ public class JSONModificationTest {
     @DisplayName("Eliminación exitosa")
     public void eliminarEmpleado() throws Exception {
 
-        /* OPCION 1 DE PRUEBA ESCRIBIENDO NUESTROS PROPIOS EMPLEADOS PARA UN ASSERTEQUALS CON EL STRING ESPECÍFICO DEL EMPLEADO QUE ELIMINAMOS
-        ////////////////////////////////////////////////////////////////////////////////////////////
+        // OPCION 1 DE PRUEBA ESCRIBIENDO NUESTROS PROPIOS EMPLEADOS PARA UN ASSERTEQUALS CON EL STRING ESPECÍFICO DEL EMPLEADO QUE ELIMINAMOS
         ArrayList<Empleado> arrayEmpleados = new ArrayList<Empleado>();
         Empleado nuevoEmpleado;
         nuevoEmpleado = new Empleado("0", "Emanuel", "Chavez", "https://jsonformatter.org/img/tom-cruise.jpg");
         arrayEmpleados.add(nuevoEmpleado);
         nuevoEmpleado = new Empleado("1", "Jamart", "Chavez", "https://jsonformatter.org/img/tom-cruise.jpg");
         arrayEmpleados.add(nuevoEmpleado);
-        nuevoEmpleado = new Empleado("2", "Garci", "Chavez", "https://jsonformatter.org/img/tom-cruise.jpg");
+        nuevoEmpleado = new Empleado("2", "Tom", "Cruise", "https://jsonformatter.org/img/tom-cruise.jpg");
         arrayEmpleados.add(nuevoEmpleado);
-        jsonReader.modifyEmployee(arrayEmpleados);
-        //////////////////////////////////////////////////////////////////////////////////////////////*/
+        //jsonReader.modifyEmployee(arrayEmpleados);
 
-        //OPCIÓN DOS DE PRUEBA CON LOS EMPLEADOS DEL JSON DE TRES ACTORES PARA UN ASSERTEQUALS DE ENTEROS DEL TOTAL DE EMPLEADOS EN EL JSON
         jsonReader.convertJSONToObjects();//se lee el json
 
-        ArrayList<Empleado> nuevoArray = new ArrayList<Empleado>();
-        nuevoArray = jsonReader.empManager.getAllContacts();//se crea un arraylist con los empleados del json
-
-        int totalEmpleadosAntes = nuevoArray.size();//se cuentan los empleados
-        jsonReader.deleteEmployee(nuevoArray, 1);//se elimina el empleado con el indice uno (trabajamos con indices por la vista enlistada)
+        int totalEmpleadosAntes = arrayEmpleados.size();//se cuentan los empleados
+        jsonReader.deleteEmployee(arrayEmpleados, 1);//se elimina el empleado con el indice uno (trabajamos con indices por la vista enlistada)
 
         jsonReader.convertJSONToObjects();//se lee de nuevo el json con el empleado eliminado
-        nuevoArray= jsonReader.empManager.getAllContacts();//se crea un arraylist con los empleados del json
-        int totalEmpleadosDespues = nuevoArray.size();//se cuentan los empleados
 
-        assertEquals(totalEmpleadosAntes, totalEmpleadosDespues + 1);//la pruba no debe ser exitosa a menos que se le sume +1 a la segunda variable
+        int totalEmpleadosDespues = arrayEmpleados.size();//se cuentan los empleados
 
-        //Si se usa la opción 1 de prueba se descomentan las lineas: 50-58 y 78 y se comenta la línea 74
+        assertEquals(totalEmpleadosAntes, totalEmpleadosDespues +1);//la pruba no debe ser exitosa a menos que se le sume +1 a la segunda variable
 
-        //assertEquals("Jamart", nuevoArray.get(1).getFirstName());//La prueba será éxitosa cuando se comente la linea de eliminación (68) y será fallida si se elimina el empleado
+        assertNotEquals("Jamart", arrayEmpleados.get(1).getFirstName());//La prueba será éxitosa cuando se comente la linea de eliminación (68) y será fallida si se elimina el empleado
 
     }
 
